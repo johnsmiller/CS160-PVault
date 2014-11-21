f135a79f2d18495fae6d7ea85c21245caf2b05d7
John MIller <computerman790@gmail.com>
2014-11-21 00:54:34 -0800

    Restart

---
+++ PVault/app/src/main/java/com/sjsu/techknowgeek/pvault/ServerConnection.java
@@ -0,0 +1,38 @@
+package com.sjsu.techknowgeek.pvault;
+
+/**
+ * Created by John on 11/20/2014.
+ */
+public class ServerConnection {
+    protected static boolean userCreate()
+    {
+
+    }
+
+    protected static boolean userLogin()
+    {
+
+    }
+
+    protected static boolean fileUpload()
+    {
+
+    }
+
+    protected static boolean fileRename()
+    {
+
+    }
+
+    protected static boolean fileDelete()
+    {
+
+    }
+
+    protected static Object[] fileRestore()
+    {
+
+    }
+
+
+}
